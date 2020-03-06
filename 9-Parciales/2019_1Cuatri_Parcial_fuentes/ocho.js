efedd4bd32d32c35da93403dbb8b9528210556a4
wmartinez23 <am3-doffy@outlook.es>
2020-03-06 16:23:28 -0300

    parcial 2019 7 y 8

--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,70 @@
 function mostrar()
 {
+    var numero;
+    var letra; 
+    var numerosPar = 0;
+    var numerosImpar = 0;
+    var numerosCero = 0;
+    var acumuladorPositivos = 0;
+    var promedio;
+    var acumuladorNegativos = 0;
+    var numeroMin = 100;
+    var letraMin;
+    var numeroMax = -100;
+    var letraMax;
+    var cantidadOperaciones = 0;
+    var continuar;
 
+    do{
+        numero = parseInt(prompt("Ingrese un número (-100/100):"));
+        while(numero < -100 || numero > 100 || isNaN(numero)){
+            numero = prompt("Número invalido. Ingrese un número (-100/100):");
+        }
+        letra = prompt("Ingrese una letra:");
+        while(!((letra >= 'A' && letra <= 'Z') || (letra >= 'a' && letra <= 'z'))){ 
+            letra = prompt("No es una letra. Ingrese una letra:");
+        }
+        // A & B
+        if(numero % 2 == 0){
+            numerosPar++;
+        } else{
+            numerosImpar++;
+        }
+        // END A & B
+
+        // C 
+        if(numero == 0){
+            numerosCero++;
+        }
+        // END C
+
+        // D & E
+        if(numero > 0){
+            acumuladorPositivos = parseInt(acumuladorPositivos + numero);
+        } else {
+            acumuladorNegativos = parseInt(acumuladorNegativos + numero);
+        }
+        // END D & E
+
+        // F
+        if(numero > numeroMax){
+            numeroMax = numero;
+            letraMax = letra;
+        }
+        if(numero < numeroMin){
+            numeroMin = numero;
+            letraMin = letra;
+        }
+        // END F
+        
+        cantidadOperaciones++;
+        continuar = prompt("Desea continuar? (s/n): ");
+    } while(continuar == "s");
+
+    // Operación D
+    promedio = parseInt(acumuladorPositivos / cantidadOperaciones);
+
+    document.write("A) " + parseInt(numerosPar) + "<br>" + "B) " + numerosImpar + "<br>" + "C) " + numerosCero + "<br>" +
+    "D) " + promedio + "<br>" + "E) " + acumuladorNegativos + "<br>" + "F) Máximo: " + numeroMax + 
+    " " + letraMax + " y Minimo: " + numeroMin + " " + letraMin + "<br>");
 }
--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,45 @@
 function mostrar()
 {
+    var altura = 0;
+    var sexo = "";
+    var sumaAlturas = 0;
+    var alturaBaja = 250;
+    var promedioAltura;
+    var sexoBajo;
+    var cantidadMujeres = 0;
 
+    for(var i = 0; i < 5 ; i++){
+        altura = parseInt(prompt("Ingrese altura en Centimetros (0/250): "));
+        while(altura <= 0 || altura > 250){
+            altura = prompt("Altura invalida. Ingrese altura en Centimetros (0/250): ");
+        }
+        sexo = prompt("Ingrese sexo (f/m): ");
+        while(sexo != "f" && sexo != "m"){
+            sexo = prompt("Sexo invalido. Ingrese sexo (f/m): ");
+        }
+        
+        // A
+        sumaAlturas = parseInt(sumaAlturas + altura);
+        // END A
+
+        // B
+        if(altura < alturaBaja){
+            alturaBaja = altura;
+            sexoBajo = sexo;
+        }
+        // END B 
+
+        // C
+        
+        if(sexo == "f" && altura > 190){
+            cantidadMujeres++;
+        }
+        // END C
+    }
+    // Operacion A
+    promedioAltura = sumaAlturas / 5;
+
+    document.write("A) El promedio de las alturas totales es: " + promedioAltura + "<br/>" +
+    "B) La altura más baja es: " + alturaBaja + " y su sexo es: " + sexoBajo + "<br/>" +
+    "C) La cantidad de mujeres que su altura supere los 190cm es: " + cantidadMujeres);
 }
